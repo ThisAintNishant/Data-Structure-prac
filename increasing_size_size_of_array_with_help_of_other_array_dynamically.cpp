36a88417ab20a675d03bb59b8d5ea7df5e148577
Nishant Gangurde <gangurdenishant07@gmail.com>
2023-04-20 20:33:16 +0530

    new file

---
+++ increasing_size_size_of_array_with_help_of_other_array_dynamically.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int *p=new int[5];               // |1|2|3|4|5|
+    int *q=new int[10];              // | | | | | | | | | | |
+    
+    p[0]=1;
+    p[1]=2;
+    p[2]=3;
+    p[3]=4;
+    p[4]=5;
+
+    for (i=0; i<5; i++)
+    {
+        q[i]=p[i];                    // |1|2|3|4|5| | | | | |
+    }
+
+    p=q;
+    q=NULL;
+
+    for (i=0; i<10; i++)
+    {
+        cout<<p[i]<<endl;
+    }
+    delete [] q;
+}
